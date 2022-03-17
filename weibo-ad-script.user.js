e06d8e95dcef96dd35487baed96fa537f646e87a
iamxiyang <iamxiyang@foxmail.com>
2022-03-17 15:29:43 +0800

    feat: 增加更新地址。该脚本功能已失效

--- weibo-ad-script.user.js
+++ weibo-ad-script.user.js
@@ -4,6 +4,9 @@
 // @version 0.2.0
 // @description 新版PC微博采取了虚拟列表，使用广告过滤工具会显示很多空白，这个脚本通过监听滚动，自动把可能出现的广告点击x->不感兴趣
 // @author hexiyang
+// @homepage https://hexiyang.cn
+// @updateURL https://github.com/iamxiyang/user-script/raw/master/weibo-ad-script.user.js
+// @downloadURL https://github.com/iamxiyang/user-script/raw/master/weibo-ad-script.user.js
 // @connect www.weibo.com
 // @include *://weibo.com/*
 // @include *://www.weibo.com/*
@@ -12,9 +15,8 @@
 
 // ==/UserScript==
 $(function () {
-
   // debug模式会在控制台输出log
-  const IS_DEBUG = false;
+  const IS_DEBUG = true
 
   // 输出日志
   const log = (content) => {
@@ -27,22 +29,21 @@ $(function () {
   const closeWeiboAd = () => {
     // 寻找微博广告
 
-    const ads = $('.vue-recycle-scroller__item-wrapper > div > div > article > div > header > div.woo-box-flex > div > span > div > i');
+    const ads = $('.vue-recycle-scroller__item-wrapper > div > div > article > div > header > div.woo-box-flex > div > span > div > i')
     if (ads.length > 0) {
       for (let i = 0, len = ads.length; i < len; i++) {
         // log($(ads[i]).attr('title'))
         if ($(ads[i]).attr('title') === '负反馈') {
-          $(ads[i]).parents('.morepop_moreIcon_1RvP9').click();
+          $(ads[i]).parents('.morepop_moreIcon_1RvP9').click()
           setTimeout(() => {
-            $(parent).find('.woo-pop-wrap-main div').first().click();
+            $(parent).find('.woo-pop-wrap-main div').first().click()
             log('已关闭广告1条')
           })
         }
       }
     }
-  };
+  }
 
   // 避免在滚动时过分触发
-  jQuery(window).on('scroll', _.throttle(closeWeiboAd, 800));
-
-});
+  jQuery(window).on('scroll', _.throttle(closeWeiboAd, 800))
+})
